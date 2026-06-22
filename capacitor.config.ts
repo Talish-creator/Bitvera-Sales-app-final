cfec7e713c3bcaf0b849d33520146f9aab381b64
Talish-creator <jsttalish@gmail.com>
2026-06-22 23:38:34 +0530

    security: add privacy screen and CSP headers

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,13 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.bitvera.salesapp',
   appName: 'Bitvera Sales',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    PrivacyScreen: {
+      enable: true,
+      imageName: "Splash"
+    }
+  }
 };
 
 export default config;
